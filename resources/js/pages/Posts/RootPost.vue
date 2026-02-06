5a1c3536634b6312522a7789d2c9f9eddf1681d7
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-02-06 16:18:17 -0300

    feat: adding page root

---
+++ resources/js/pages/Posts/RootPost.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+import { home } from '@/routes';
+
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+
+<template>
+    <Head title="Post">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+
+    <header>
+        <Link :href="home()">Root</Link>
+    </header>
+
+
+    <h2>UMA PARADA MUITO LOUCA MESMO</h2>
+</template>
