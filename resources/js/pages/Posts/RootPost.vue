2c40fa2de548f5ee43e0393e4adb36951693b996
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-02-10 13:10:07 -0300

    UI: adding styles for list post

--- resources/js/pages/Posts/RootPost.vue
+++ resources/js/pages/Posts/RootPost.vue
@@ -22,5 +22,14 @@ defineProps({
 
     <Header />
 
-    <h2>UMA PARADA MUITO LOUCA MESMO</h2>
+    <section class="w-full h-[90vh] flex flex-col items-center gap-10">
+        <h2>Pagina principal dos posts depois de logado no carai a 4</h2>
+
+        <div v-for="post in posts" :key="post.id" class="w-[20%] h-[20%] border-2 border-gray-200 rounded-xl flex flex-col items-center justify-center">
+            <p>Number id {{ post.id }}</p>
+            <p>Title: {{ post.title }}</p>
+            <p>Description: {{ post.description }}</p>
+        </div>
+
+    </section>
 </template>
