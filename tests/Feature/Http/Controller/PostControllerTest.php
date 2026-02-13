3fbd73ac2fb39f7dbd02833f36123e2a74fff053
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-02-13 16:04:13 -0300

    test: adding anothers tests for controllers

--- tests/Feature/Http/Controller/PostControllerTest.php
+++ tests/Feature/Http/Controller/PostControllerTest.php
@@ -6,13 +6,15 @@ use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 test('returns posts index', function () {
-    /** @var User $user */
+
     $user = User::factory()->createOne();
 
     Post::factory(3)->for($user)->create();
 
+    /** @var User $user */
     actingAs($user);
 
     get(route('posts.index'))
@@ -25,4 +27,48 @@ test('returns posts index', function () {
 
 test('Redirect to login page if not login user', function () {
     get(route('posts.index'))->assertRedirect(route('login'));
+});
+
+test('cannot create post with invalid data', function () {
+    $user = User::factory()->create();
+
+    /** @var User $user */
+    actingAs($user);
+
+    post(route('posts.store'), [
+        'title' => '',
+        'description' => '',
+    ])->assertSessionHasErrors(['title', 'description']);
+});
+
+test('can view own post', function () {
+    $user = User::factory()->create();
+    $post = Post::factory()->for($user)->createOne();
+    /** @var User $user */
+    actingAs($user);
+
+    get(route('posts.show', $post))
+        ->assertOk()
+        ->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Posts/ShowPost')
+            ->has('post', fn (AssertableInertia $prop) => $prop
+                ->where('id', $post->id)
+                ->where('title', $post->title)
+                ->where('description', $post->description)
+                ->etc()
+            )
+        );
+});
+
+test('cannot view post another user', function () {
+    $user = User::factory()->create();
+    $anotherUser = User::factory()->create();
+
+    $post = Post::factory()->for($anotherUser)->createOne();
+
+    /** @var User $user */
+    actingAs($user);
+
+    get(route('posts.show', $post))
+        ->assertForbidden();
 });
