f1c922d1eb907b1c1af295cf46a9be4e9ffbb982
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-02-16 14:27:56 -0300

    refact: changing posts controller test

--- tests/Feature/Http/Controller/PostControllerTest.php
+++ tests/Feature/Http/Controller/PostControllerTest.php
@@ -11,123 +11,137 @@ use function Pest\Laravel\get;
 use function Pest\Laravel\post;
 use function Pest\Laravel\put;
 
-test('returns posts index', function () {
+describe('Vies posts tests', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+    });
 
-    $user = User::factory()->createOne();
+    test('returns posts index', function () {
+        Post::factory(3)->for($this->user)->create();
 
-    Post::factory(3)->for($user)->create();
+        actingAs($this->user);
 
-    /** @var User $user */
-    actingAs($user);
+        get(route(name: 'posts.index'))
+            ->assertOk()
+            ->assertInertia(fn (AssertableInertia $page) => $page
+                ->component('Posts/RootPost')
+                ->has('posts', 3)
+            );
+    });
 
-    get(route('posts.index'))
-        ->assertOk()
-        ->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Posts/RootPost')
-            ->has('posts', 3)
-        );
+    test('can view own post', function () {
+        $post = Post::factory()->for($this->user)->createOne();
+
+        /** @var User $user */
+        actingAs($this->user);
+
+        get(route('posts.show', $post))
+            ->assertOk()
+            ->assertInertia(fn (AssertableInertia $page) => $page
+                ->component('Posts/ShowPost')
+                ->has('post', fn (AssertableInertia $prop) => $prop
+                    ->where('id', $post->id)
+                    ->where('title', $post->title)
+                    ->where('description', $post->description)
+                    ->etc()
+                )
+            );
+    });
+
+    test('cannot view post another user', function () {
+        $anotherUser = User::factory()->create();
+
+        $post = Post::factory()->for($anotherUser)->createOne();
+
+        /** @var User $user */
+        actingAs($this->user);
+
+        get(route('posts.show', $post))
+            ->assertForbidden();
+    });
 });
 
 test('Redirect to login page if not login user', function () {
     get(route('posts.index'))->assertRedirect(route('login'));
 });
 
-test('cannot create post with invalid data', function () {
-    $user = User::factory()->create();
+describe('Create Tests', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+    });
 
-    /** @var User $user */
-    actingAs($user);
+    test('cannot create post with invalid data', function () {
+        /** @var User $user */
+        actingAs($this->user);
 
-    post(route('posts.store'), [
-        'title' => '',
-        'description' => '',
-    ])->assertSessionHasErrors(['title', 'description']);
+        post(route('posts.store'), [
+            'title' => '',
+            'description' => '',
+        ])->assertSessionHasErrors(['title', 'description']);
+    });
 });
 
-test('can view own post', function () {
-    $user = User::factory()->create();
-    $post = Post::factory()->for($user)->createOne();
-    /** @var User $user */
-    actingAs($user);
+describe('Update Tests', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+    });
 
-    get(route('posts.show', $post))
-        ->assertOk()
-        ->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Posts/ShowPost')
-            ->has('post', fn (AssertableInertia $prop) => $prop
-                ->where('id', $post->id)
-                ->where('title', $post->title)
-                ->where('description', $post->description)
-                ->etc()
-            )
-        );
+    test('can update your own post', function () {
+        $post = Post::factory()->for($this->user)->create();
+
+        /** @var User $user */
+        actingAs($this->user);
+
+        put(route('posts.update', $post), [
+            'title' => 'New Title',
+            'description' => 'New description', ])
+            ->assertRedirect(route('posts.index'))
+            ->assertSessionHas('success', 'Post Atualizado com sucesso');
+
+        assertDatabaseHas('posts', ['title' => 'New Title']);
+    });
+
+    test('cannot update your own post', function () {
+        $anotherUser = User::factory()->create();
+
+        $post = Post::factory()->for($anotherUser)->createOne();
+
+        /** @var User $user */
+        actingAs($this->user);
+
+        put(route('posts.update', $post), [
+            'title' => 'Other title',
+            'description' => 'Other description'])
+            ->assertForbidden();
+    });
 });
 
-test('cannot view post another user', function () {
-    $user = User::factory()->create();
-    $anotherUser = User::factory()->create();
+describe('Delete Tests', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+    });
 
-    $post = Post::factory()->for($anotherUser)->createOne();
+    test('can you delete your own post', function () {
+        $post = Post::factory()->for($this->user)->create();
 
-    /** @var User $user */
-    actingAs($user);
+        /** @var User $user */
+        actingAs($this->user);
 
-    get(route('posts.show', $post))
-        ->assertForbidden();
-});
+        delete(route('posts.destroy', $post))
+            ->assertRedirect(route('posts.index'))
+            ->assertSessionHas('success', 'Post Deletado com sucesso');
+    });
 
-test('can update your own post', function () {
-    $user = User::factory()->create();
-    $post = Post::factory()->for($user)->create();
+    test('cannot delete other post', function () {
+        $anotherUser = User::factory()->create();
+        $post = Post::factory()->for($anotherUser)->createOne();
 
-    /** @var User $user */
-    actingAs($user);
+        /** @var User $user */
+        actingAs($this->user);
 
-    put(route('posts.update', $post), [
-        'title' => 'New Title',
-        'description' => 'New description', ])
-        ->assertRedirect(route('posts.index'))
-        ->assertSessionHas('success', 'Post Atualizado com sucesso');
+        delete(route('posts.update', $post))
+            ->assertRedirect(route('posts.index'))
+            ->assertSessionHas('success', 'Post Deletado com sucesso');
+    });
 
-    assertDatabaseHas('posts', ['title' => 'New Title']);
-});
-
-test('cannot update your own post', function () {
-    $user = User::factory()->create();
-    $anotherUser = User::factory()->create();
-
-    $post = Post::factory()->for($anotherUser)->createOne();
-
-    /** @var User $user */
-    actingAs($user);
-
-    put(route('posts.update', $post), [
-        'title' => 'Other title',
-        'description' => 'Other description'])
-        ->assertForbidden();
-});
-
-test('can you delete your own post', function () {
-    $user = User::factory()->create();
-    $post = Post::factory()->for($user)->create();
-
-    /** @var User $user */
-    actingAs($user);
-
-    delete(route('posts.destroy', $post))
-        ->assertRedirect(route('posts.index'))
-        ->assertSessionHas('success', 'Post Deletado com sucesso');
-});
-
-test('cannot delete other post', function () {
-    $user = User::factory()->create();
-    $anotherUser = User::factory()->create();
-    $post = Post::factory()->for($anotherUser)->createOne();
-
-    /** @var User $user */
-    actingAs($user);
-
-    delete(route('posts.update', $post))
-        ->assertRedirect(route('posts.index'))
-        ->assertSessionHas('success', 'Post Deletado com sucesso');
 });
