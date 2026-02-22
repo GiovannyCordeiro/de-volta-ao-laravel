0dc2aea48570264e92403affc771afae0ed95f28
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2026-02-22 13:11:16 -0300

    build: setup configurations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
+    use Billable;
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
